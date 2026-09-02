6a4d8d47a5a42ebeaf674cf03320c840d87c0d27
leoamirstar-cmd <leoamirstar@gmail.com>
2026-09-02 20:34:03 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/io_client.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'services/gemini_manager.dart';
 
 void main() {
   runApp(const NamiraApp());
