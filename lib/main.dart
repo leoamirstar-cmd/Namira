fba1d36b7840aa605394f09f3d9bfa63747a493f
leoamirstar-cmd <leoamirstar@gmail.com>
2026-09-02 08:39:36 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -103,7 +103,7 @@ class _ChatScreenState extends State<ChatScreen> {
           "Authorization": "Bearer $apiKey",
         },
         body: jsonEncode({
-          "model": "llama-3.3-70b-versatile",
+        "model": "llama-3.1-8b-instant",
           "messages": [{"role": "user", "content": text}]
         }),
       );
