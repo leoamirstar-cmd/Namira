f28df59f54194665abb2f61a2847884d10d08e14
leoamirstar-cmd <leoamirstar@gmail.com>
2026-09-05 08:56:15 +0330

    Update music_screen.dart

--- lib/screens/music_screen.dart
+++ lib/screens/music_screen.dart
@@ -23,7 +23,7 @@ class _MusicScreenState extends State<MusicScreen> {
   final AudioPlayer _audioPlayer = AudioPlayer();
   String? _currentlyPlayingUrl;
   
-  final String _serverBaseUrl = 'https://music-extractor.onrender.com';
+  final String _serverBaseUrl = 'https://namira-music-api.leoamirstar.workers.dev';
 
   @override
   void initState() {
